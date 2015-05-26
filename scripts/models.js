d716f0b854274c6ad3d400b28cce9261cf010ddf
Jessy Riordan <jessy.riordan@gmail.com>
2015-05-26 17:28:49 -0400

    making progress... i hope

---
+++ scripts/models.js
@@ -0,0 +1,21 @@
+var chessboard = (function(window) {
+
+  function initBoard() {
+    return [
+      ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+      ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+      ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
+    ];
+  }
+
+  function move(board, fromX, fromY, toX, toY) {
+    
+  }
+
+
+})(window);
