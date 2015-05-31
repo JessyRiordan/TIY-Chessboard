77c72d25e5fc6983fa8d0cc18b28c5c06751590d
Jessy Riordan <jessy.riordan@gmail.com>
2015-05-31 15:50:25 -0400

    I have 1 more down in my main.js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("poop")
+(function(window) {
+
+  $('.next').click(function() {
+    $('.rank-2 > .file-d').removeClass('white pawn');
+    $('.rank-4 > .file-d').addClass('white pawn')
+    console.log("Yippe!");
+  });
+
+})(window);
--- scripts/models.js
+++ scripts/models.js
@@ -1,22 +1,14 @@
 (function(window) {
-  window.chess = {
+    window.chess = {
 
-    var board = [
-        ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
-        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-        ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
-      ];
+        // var board = {
+        //     rank: [1, 2, 3, 4, 5, 6, 7, 8],
+        //     file: [1, 2, 3, 4, 5, 6, 7, 8]
+        // };
+        //
+        // var piece = function(rank, file) {
+        //
+        // }
 
-    var initBoard = function initBoard() {
-      console.log(board.join('\n') + '\n\n');
-    }
-
-
-  };
-  }
+    };
 })(window);
