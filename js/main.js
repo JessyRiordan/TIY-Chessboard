6df8913a662a3d56b0ef25ea8e74a2c22eb06fc6
Jessy Riordan <jessy.riordan@gmail.com>
2015-05-31 16:45:17 -0400

    I like to move it, move it!

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 (function(window) {
 
   $('.next').click(function() {
-    $('.rank-2 > .file-d').removeClass('white pawn');
-    $('.rank-4 > .file-d').addClass('white pawn')
-    console.log("Yippe!");
+    chess.move(chess.move1);
+    // $('.rank-2 > .file-d').removeClass('white pawn');
+    // $('.rank-4 > .file-d').addClass('white pawn')
+    // console.log("Yippe!");
   });
 
 })(window);
--- scripts/models.js
+++ scripts/models.js
@@ -1,14 +1,26 @@
 (function(window) {
+
+    var move1 = ['.rank-2', '.file-d', '.rank-4', '.file-d', 'white pawn'];
+
+    function move(whichMove) {
+        $(whichMove[0] + '>' + whichMove[1]).removeClass(whichMove[4]);
+        $(whichMove[2] + '>' + whichMove[3]).addClass(whichMove[4]);
+    };
+
     window.chess = {
 
-        // var board = {
-        //     rank: [1, 2, 3, 4, 5, 6, 7, 8],
-        //     file: [1, 2, 3, 4, 5, 6, 7, 8]
-        // };
-        //
-        // var piece = function(rank, file) {
-        //
-        // }
+        'move1': move1,
+        'move': move
 
     };
 })(window);
+
+
+// var board = {
+//     rank: [1, 2, 3, 4, 5, 6, 7, 8],
+//     file: [1, 2, 3, 4, 5, 6, 7, 8]
+// };
+//
+// var piece = function(rank, file) {
+//
+// }
