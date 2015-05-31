a53d4bfa88fd48a93c5eb3bab7e014c3d5d498ec
Jessy Riordan <jessy.riordan@gmail.com>
2015-05-31 17:09:22 -0400

    I can skip ALL MY moves

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 (function(window) {
 
   $('.next').click(function() {
-    chess.move(chess.move1);
-    // $('.rank-2 > .file-d').removeClass('white pawn');
-    // $('.rank-4 > .file-d').addClass('white pawn')
-    // console.log("Yippe!");
+    chess.move(chess.game.move1);
+  });
+
+  $('.last').click(function() {
+    chess.move(chess.game.move1);
+    chess.move(chess.game.move2);
+    chess.move(chess.game.move3);
+    chess.move(chess.game.move4);
+    chess.move(chess.game.move5);
+    chess.move(chess.game.move6);
+    chess.move(chess.game.move7);
+    chess.move(chess.game.move8);
+    chess.move(chess.game.move9);
   });
 
 })(window);
+
+// $('.rank-2 > .file-d').removeClass('white pawn');
+// $('.rank-4 > .file-d').addClass('white pawn')
+// console.log("Yippe!");
--- scripts/models.js
+++ scripts/models.js
@@ -1,6 +1,16 @@
 (function(window) {
 
-    var move1 = ['.rank-2', '.file-d', '.rank-4', '.file-d', 'white pawn'];
+    var game = {
+      move1: ['.rank-2', '.file-d', '.rank-4', '.file-d', 'white pawn'],
+      move2: ['.rank-8', '.file-g', '.rank-6', '.file-f', 'black knight'],
+      move3: ['.rank-2', '.file-c', '.rank-4', '.file-c', 'white pawn'],
+      move4: ['.rank-7', '.file-e', '.rank-6', '.file-e', 'black pawn'],
+      move5: ['.rank-2', '.file-g', '.rank-3', '.file-g', 'white pawn'],
+      move6: ['.rank-7', '.file-d', '.rank-5', '.file-d', 'black pawn'],
+      move7: ['.rank-1', '.file-f', '.rank-2', '.file-g', 'white bishop'],
+      move8: ['.rank-8', '.file-f', '.rank-7', '.file-e', 'black bishop'],
+      move9: ['.rank-1', '.file-g', '.rank-3', '.file-f', 'white knight']
+   };
 
     function move(whichMove) {
         $(whichMove[0] + '>' + whichMove[1]).removeClass(whichMove[4]);
@@ -9,7 +19,7 @@
 
     window.chess = {
 
-        'move1': move1,
+        'game': game,
         'move': move
 
     };
